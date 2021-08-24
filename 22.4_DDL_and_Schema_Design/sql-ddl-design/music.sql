4018a5d647ab83d863ad862e124e97cdd1c012a2
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2021-08-23 21:13:05 -0700

    Album length float precision not working

--- 22.4_DDL_and_Schema_Design/sql-ddl-design/music.sql
+++ 22.4_DDL_and_Schema_Design/sql-ddl-design/music.sql
@@ -31,3 +31,39 @@ VALUES
   ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
   ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
   ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+
+
+  *******************************************************************************
+
+DROP DATABASE IF EXISTS music;
+
+CREATE DATABASE music;
+
+\c music
+
+CREATE TABLE songs (
+  id SERIAL PRIMARY KEY,
+  artist TEXT NOT NULL,
+  best_selling_album TEXT[],
+  album_length FLOAT NOT NULL,
+  num_of_tracks INTEGER NOT NULL,
+  num_of_albums INTEGER NOT NULL,
+  album_award BOOLEAN NOT NULL,
+  song_writers TEXT[],
+  record_company TEXT NOT NULL
+  -- copies_sold INTEGER NOT NULL
+);
+
+INSERT INTO songs (artist, best_selling_album, num_of_tracks, num_of_albums, album_award, song_writers, record_company)
+VALUES ('Beastie Boys', '{Licensed to Ill}', '13', '8', 'True', '{Beastie Boys, Rick Rubin}', 'Def Jam'),
+('Madonna', '{True Blue}', '9', '64', 'True', '{Madonna, Stephen Bray, Patrick Leonard}', 'Sire/Warner Bros'),
+('Rihanna', '{Good Girl Gone Bad}',  '12', 'True', '{Rihanna}', 'Def Jam'),
+('Keak da Sneak', '{Town Business}', '13', '36', 'False', '{Charles Bowens}', 'Moe Doe Entertainment'),
+('Michael Jackson','{Thriller}', '9', '60', 'True', '{Michael Jackson, Quincy Jones}', 'Epic');
+
+('{}', '{}', ),
+('{}', '{}', ),
+('{}', '{}', ),
+('{}', '{}', ),
+('{}', '{}', )
+
